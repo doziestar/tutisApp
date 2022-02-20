eac0db79a2c1c4633ff346266052d3333a34b86f
doziestar <chidosiky2015@gmail.com>
2022-02-20 20:18:48 +0100

    more widgets

--- lib/sec2/body.dart
+++ lib/sec2/body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tutis/sec2/transaction.dart';
+
+import '../sec2/transaction.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
@@ -32,62 +33,7 @@ class Body extends StatelessWidget {
             ),
           ),
         ),
-        Column(
-          children: [
-            ...transactions.map((tx) {
-              return Card(
-                child: Row(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      padding: const EdgeInsets.all(10),
-                      child: Text(
-                        '\$${tx.amount}',
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.purple,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          tx.title,
-                          style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: const [
-                        Icon(
-                          Icons.delete,
-                          color: Colors.red,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            }).toList(),
-          ],
-        ),
+        const TransactionWid()
       ],
     );
   }
--- lib/sec2/transaction.dart
+++ lib/sec2/transaction.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
 class Transaction {
   final String id;
   final String title;
@@ -26,3 +29,173 @@ final List<Transaction> transactions = [
     date: DateTime.now(),
   ),
 ];
+
+class ReceiveInput extends StatelessWidget {
+  final VoidCallback _addTransaction;
+
+  const ReceiveInput(this._addTransaction);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: const EdgeInsets.all(10),
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Title',
+                labelStyle: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              controller: titleController,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                labelText: 'Amount',
+                labelStyle: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              keyboardType: const TextInputType.numberWithOptions(
+                decimal: true,
+              ),
+              controller: amountController,
+            ),
+            TextButton(
+              onPressed: () {
+                _addTransaction();
+              },
+              child: const Text(
+                'Add Transaction',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// late String titleInput;
+// late String amountInput;
+final titleController = TextEditingController();
+final amountController = TextEditingController();
+
+class TransactionWid extends StatefulWidget {
+  const TransactionWid({Key? key}) : super(key: key);
+
+  @override
+  State<TransactionWid> createState() => _TransactionWidState();
+}
+
+class _TransactionWidState extends State<TransactionWid> {
+  void _addTransaction(String title, double amount) {
+    final newTransaction = Transaction(
+      id: DateTime.now().toString(),
+      title: title,
+      amount: amount,
+      date: DateTime.now(),
+    );
+    // transactions.add(newTransaction);
+    setState(() {
+      transactions.add(newTransaction);
+    });
+  }
+
+  void _deleteTransaction(String id) {
+    setState(() {
+      transactions.removeWhere((tx) => tx.id == id);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ReceiveInput(() => _addTransaction(
+            titleController.text, double.parse(amountController.text))),
+        ...transactions.map(
+          (tx) {
+            return Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              margin: const EdgeInsets.all(5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      '\$${tx.amount}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    // mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tx.title,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        DateFormat.yMMMd().add_jm().format(tx.date),
+                        style: const TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    // mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () => _deleteTransaction(tx.id),
+                        child: const Icon(Icons.delete),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          },
+        ).toList(),
+      ],
+    );
+  }
+}
